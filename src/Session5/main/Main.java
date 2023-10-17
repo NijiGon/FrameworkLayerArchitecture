434ac281f211c3285bb564e03458649e5a42143b
NijiGon <matthewarif50@gmail.com>
2023-10-18 04:59:45 +0700

    session 1-5 material

--- src/Session5/main/Main.java
+++ src/Session5/main/Main.java
@@ -5,13 +5,31 @@ import Session5.model.Archer;
 import Session5.model.Enemy;
 import Session5.model.Warrior;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public Main() {
-		Enemy enemy = new Enemy();
-		Warrior warrior = new Warrior(1, 1);
-		Archer archer = new Archer(1, 1);
-		Game game = new Game(warrior, enemy);
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true){
+			System.out.println("Game");
+			String role;
+			do{
+				System.out.print("Input Role [Warrior | Archer] : ");
+				role = sc.nextLine();
+			}while(!role.equals("Warrior") && !role.equals("Archer"));
+			Enemy enemy = new Enemy();
+			if(role.equals("Warrior")){
+				Warrior warrior = new Warrior(1, 1);
+				Game game = new Game(warrior, enemy);
+			}
+			else {
+				Archer archer = new Archer(1, 1);
+				Game game = new Game(new ArcherToMeleeAdapter(archer), enemy);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
